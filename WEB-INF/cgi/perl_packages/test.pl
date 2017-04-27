fd7340f22b4597c29a9e23f7d62b1d69959203d1
LilySnow <me.lixue@gmail.com>
2017-04-27 03:44:02 -0400

    cgi deleted

--- WEB-INF/cgi/perl_packages/test.pl
+++
@@ -1,8 +0,0 @@
-#!usr/bin/perl -w
-#use Thread;
-use Thread  qw(async);
-my $something='xue';
-for my $t (Thread->list()) {
-    printf "$t has tid = %d\n", $t->tid();
-}
-#my $t = async{ say($something);  };
